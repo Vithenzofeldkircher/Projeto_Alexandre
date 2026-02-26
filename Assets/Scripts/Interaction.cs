21551c87a948cd514160216e21de3533214da57e
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2026-02-26 10:48:14 -0300

    Update

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    private IInteractable _Target;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_Target == null)
+            return;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _Target.Active();
+            Debug.Log("ESTÁ CLICANDO");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out _Target))
+        {
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _Target = null;
+    }
+}
+
+
